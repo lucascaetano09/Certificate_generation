600453534e36b5800ad309d164cc93c6aed52a3c
GuilhermeMViana <gmunizviana@gmail.com>
2024-11-06 22:13:26 -0300

    feat: criando gerador de pdf

--- DiplomaGenerator/processor.js
+++ DiplomaGenerator/processor.js
@@ -1,92 +1,52 @@
-const Minio = require('minio');
 const puppeteer = require('puppeteer');
-const mysql = require('mysql2/promise');
-const fs = require('fs').promises;
+const fs = require('fs');
+const path = require('path');
+const { v4: uuidv4 } = require("uuid");
+const uuid = uuidv4();
 
-class DiplomaProcessor {
-  constructor(context, minioClient) {
-    this.context = context;
-    this.minioClient = minioClient;
-  }
 
-  async processMessage(message) {
-    const diplomaData = JSON.parse(message);
-
-    console.log(`Processando diploma para ${diplomaData.nomeAluno}`);
-
-    const templatePath = 'template-diploma.html';
-    let templateHtml = await fs.readFile(templatePath, 'utf8');
-    templateHtml = templateHtml.replace('{{nome_aluno}}', diplomaData.nomeAluno)
-                               .replace('{{curso}}', diplomaData.curso)
-                               .replace('{{data_conclusao}}', new Date(diplomaData.dataConclusao).toLocaleDateString('pt-BR'));
-
-    const browser = await puppeteer.launch({
-      executablePath: '/usr/bin/google-chrome-stable',
-      headless: true,
-      args: ['--no-sandbox', '--disable-setuid-sandbox']
-    });
-
-    const page = await browser.newPage();
-    await page.setContent(templateHtml);
-
-    const pdfBytes = await page.pdf({
-      format: 'A4',
-      printBackground: true
-    });
-
-    await browser.close();
-
-    const pdfFilePath = `diplomas/${diplomaData.nomeAluno}_${new Date(diplomaData.dataConclusao).toISOString().split('T')[0]}.pdf`;
-
-    await this.minioClient.putObject('diplomas', pdfFilePath, pdfBytes);
-
-    const diploma = {
-      nomeAluno: diplomaData.nomeAluno,
-      curso: diplomaData.curso,
-      dataConclusao: diplomaData.dataConclusao,
-      dataEmissao: new Date()
-    };
-
-    await this.context.execute(
-      'INSERT INTO diplomas (nome_aluno, curso, data_conclusao, data_emissao) VALUES (?, ?, ?, ?)',
-      [diploma.nomeAluno, diploma.curso, diploma.dataConclusao, diploma.dataEmissao]
-    );
-
-    console.log(`Diploma gerado e salvo em ${pdfFilePath}`);
-  }
+async function gerarPDF(html) {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.setContent(html);
+  const pdf = await page.pdf({ format: 'A4', landscape: true });
+  await browser.close();
+  return pdf;
 }
 
-const minioClient = new Minio.Client({
-  endPoint: 'localhost',
-  port: 9000,
-  useSSL: false,
-  accessKey: 'YOUR_MINIO_ACCESS_KEY',
-  secretKey: 'YOUR_MINIO_SECRET_KEY'
-});
-
-const mysqlConfig = {
-  host: 'localhost',
-  user: 'root',
-  password: 'YOUR_MYSQL_PASSWORD',
-  database: 'sistema_diplomas'
-};
-
-async function main() {
-  const connection = await mysql.createConnection(mysqlConfig);
-  const diplomaProcessor = new DiplomaProcessor(connection, minioClient);
-
-  // Exemplo de uso da função (substitua `message` pelo JSON da mensagem a ser processada)
-  const message = JSON.stringify({
-    nomeAluno: 'João Silva',
-    curso: 'Engenharia de Software',
-    dataConclusao: '2024-06-15'
-  });
-
-  try {
-    await diplomaProcessor.processMessage(message);
-  } finally {
-    await connection.end();
-  }
+function template(filePath) {
+  return fs.readFileSync(filePath, 'utf-8');
 }
 
-main().catch(console.error);
+function substituirDados(html, dados) {
+  return Object.entries(dados).reduce((conteudo, [dados, value]) => {
+    return conteudo.replace(new RegExp(dados, 'g'), value);
+  }, html);
+}
+
+(async () => {
+  const diplomaTemplate = path.join(__dirname, 'template.html');
+  const pdfGerado = path.join(__dirname, '../diplomasGerados', `${uuid}.pdf`);
+  
+  const diplomaHTML = template(diplomaTemplate);
+
+  const jsonTeste = {
+    "{{nome_aluno}}": "Guilherme Viana",
+    "{{nacionalidade}}": "Brasileiro",
+    "{{estado}}": "São Paulo",
+    "{{data_nascimento}}": "07/03/2003",
+    "{{documento}}": "92954",
+    "{{data_conclusao}}": "01/01/0001",
+    "{{curso}}": "Sistemas de Informação",
+    "{{carga_horaria}}": "4000",
+    "{{data_emissao}}": "06/11/2024",
+    "{{nome_assinatura}}": "José Romualdo",
+    "{{cargo}}": "Professor"
+  };
+
+  const conteudoFinal = substituirDados(diplomaHTML, jsonTeste);
+  const pdfBuffer = await gerarPDF(conteudoFinal);
+  
+  fs.writeFileSync(pdfGerado, pdfBuffer);
+  console.log('PDF gerado com sucesso:', pdfGerado);
+})().catch(console.error);
